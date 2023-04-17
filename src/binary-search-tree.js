cde95a46ea58cd9dadcace866857b1342455c876
Vanyha010 <vanyha010@icloud.com>
2023-04-17 21:07:11 +0300

    add binary-search-tree solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,140 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
+  constructor() {
+    this.treeRoot = null;
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.treeRoot;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.treeRoot = addElement(this.treeRoot, data);
+
+    function addElement(node, data) {
+      if(!node) {
+        node = new Node(data);
+        return node;
+      }
+
+      if(node.data === data) {
+        return node;
+      }
+
+      if(node.data < data) {
+        node.rigth = addElement(node.rigth, data);
+      } else {
+        node.left = addElement(node.left, data);
+      }
+
+      return node;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return checkElementExistence(this.treeRoot, data);
+    
+    function checkElementExistence(node, data) {
+      if (!node) {
+        return false;
+      }
+
+      if(node.data === data) {
+        return true;
+      }
+
+      if(node.data > data) {
+        return checkElementExistence(node.left, data);
+      } else {
+        return checkElementExistence(node.rigth, data);
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return findNode(this.treeRoot, data);
+    function findNode(node, data) {
+      if (!node) {
+        return null;
+      }
+      if(node.data === data) {
+        return node;
+      }
+      if (node.data > data) {
+        return findNode(node.left, data);
+      } else {
+        return findNode(node.rigth, data);
+      }
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.treeRoot = removeElement(this.treeRoot, data);
+    function removeElement(node, data) {
+      if(!node) {
+        return null;
+      }
+      if (data > node.data) {
+        node.rigth = removeElement(node.rigth, data);
+        return node;
+      } else if (data < node.data) {
+        node.left = removeElement(node.left, data);
+        return node;
+      } else {
+        if (!node.left && !node.rigth) {
+          return null;
+        }
+
+        if(!node.rigth) {
+          node = node.left;
+          return node;
+        }
+
+        if(!node.left) {
+          node = node.rigth;
+          return node;
+        }
+
+        let maxFromLeft = node.left;
+        while(maxFromLeft.rigth) {
+          maxFromLeft = maxFromLeft.rigth;
+        }
+        node.data = maxFromLeft.data;
+        node.left = removeElement(node.left, maxFromLeft.data);
+
+        return node;
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(!this.treeRoot) {
+      return null;
+    }
+
+    let node = this.treeRoot;
+    while (node.left) {
+      node = node.left;
+    }
+    return node.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(!this.treeRoot) {
+      return null;
+    }
+
+    let node = this.treeRoot;
+    while (node.rigth) {
+      node = node.rigth;
+    }
+    return node.data;
   }
 }
 
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -23,8 +23,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
 }
 
 module.exports = {
